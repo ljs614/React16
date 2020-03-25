c13594cd14bdcd97c8f57201a192398d70526b24
ljs614 <sjl0614@naver.com>
2020-03-25 22:40:50 +0900

    Fragments and Return Strings

--- master-react16/src/App.js
+++ master-react16/src/App.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+class ReturnTypes extends Component {
+  render() {
+    // return (
+    //   <Fragment>
+    //     <header></header>
+    //     <div></div>
+    //     <footer></footer>
+    //   </Fragment>
+    // )
+    return "hello"; //return String
+  }
+}
 
 class App extends Component {
   render() {
-    return <div className="App"/>
+    return ( <Fragment>
+      <ReturnTypes/>
+    </Fragment>)
   }
 }
 
